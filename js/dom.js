b7ee77359a8f4a9fbe97194c3d7299097602482e
Rafael Ketelhohn <rafaelk@gmail.com>
2022-07-07 17:32:19 -0300

    primera instancia luego de la incorporación de librerías

---
+++ js/dom.js
@@ -0,0 +1,376 @@
+//entrega martes 21-06 Modificando el HTML desde js
+
+//Acá modifico el titulo h1 (le cambio el texto original)
+const titulo = document.getElementById('main'); 
+console.log(titulo.innerText);
+titulo.innerHTML = '<h1>Ketel Old School Skateboard Shop</h1>';
+console.log(titulo.innerText);
+titulo.hidden = true; //escondo título
+titulo.hidden = false; //vuelvo a mostrar título
+
+//Acá modifico el subtitulo de H3 a H4 y le cambio el texto original
+const subTitulo = document.getElementById('descripcion'); 
+console.log(subTitulo.innerText);
+subTitulo.innerHTML = '<h4>A continuación Ud. podrá ver los productos disponibles en nuestra tienda</h4>';
+console.log(subTitulo.innerText);
+
+//Acá traigo los 9 skaters (H2)
+const skaters = document.getElementsByTagName('h2'); 
+console.log(skaters);
+console.log(skaters[0]);//traigo el nombre de uno de los skaters
+console.log(skaters[2]);//otro
+console.log(skaters[8]);//y otro
+
+//traigo elementos por clase creando un array
+const profesionales = document.getElementsByClassName('cards'); 
+console.log(profesionales);
+
+for (const profesional of profesionales) {
+    console.log(profesional);
+    //profesional.innerHTML = '<h2>AHORA SON todos WHISKIES y RONES</h2>';
+  }//si activo lo de arriba modifico todos los cards
+
+let elementByQueryId = document.querySelector('#descripcion');
+let elementByQueryClass = document.querySelector('.parrafito');
+let elementsByQueryClass = document.querySelectorAll('.nombre');
+
+console.log(elementByQueryId);
+console.log(elementByQueryClass);
+console.log(elementsByQueryClass);
+
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Santa Cruz Dollar Flame',
+        descripcion:'Santa Cruz Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 17999,
+        imagen: './images/SKATESANTACRUZ1.png'
+    },
+    {
+        id: 2,
+        nombre: 'Santa Cruz Crane Dot',
+        descripcion:'Santa Cruz Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 24999,
+        imagen: './images/SKATESANTACRUZ2.png'
+    },
+    {
+        id: 3,
+        nombre: 'Santa Cruz Rad Dot',
+        descripcion:'Santa Cruz Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 24999,
+        imagen: './images/SKATESANTACRUZ3.png'
+    },
+    {
+        id: 4,
+        nombre: 'Santa Cruz Lucky Cat',
+        descripcion:'Santa Cruz Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 17999,
+        imagen: './images/SKATESANTACRUZ4.png'
+    },
+    {
+        id: 5,
+        nombre: 'Santa Cruz Wooten',
+        descripcion:'Santa Cruz Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 17999,
+        imagen: './images/SKATESANTACRUZ5.png'
+    },
+    {
+        id: 6,
+        nombre: 'Creature Erosion',
+        descripcion:'Creature Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 24999,
+        imagen: './images/SKATECREATURE1.png'
+    },
+    {
+        id: 7,
+        nombre: 'Creature Russell Skull',
+        descripcion:'Creature Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 17999,
+        imagen: './images/SKATECREATURE2.png'
+    },
+    {
+        id: 8,
+        nombre: 'Creature Russell',
+        descripcion:'Creature Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 17999,
+        imagen: './images/SKATECREATURE3.png'
+    },
+    {
+        id: 9,
+        nombre: 'Creature Martinez',
+        descripcion:'Creature Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 17999,
+        imagen: './images/SKATECREATURE4.png'
+    },
+    {
+        id: 10,
+        nombre: 'Creature Deathcard',
+        descripcion:'Creature Deck, Fabricada con 7 capas de maple canadiense prensado individualmente para una mayor resistencia de la misma',
+        precio: 24999,
+        imagen: './images/SKATECREATURE5.png'
+    },
+    {
+        id: 11,
+        nombre: 'Santa Cruz Flier Hand',
+        descripcion:'Santa Cruz Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+        precio: 34999,
+        imagen: './images/COMPLETESANTACRUZ1.png'
+    },
+    {
+        id: 12,
+        nombre: 'Santa Cruz Screaming ',
+        descripcion:'Santa Cruz Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+        precio: 34999,
+        imagen: './images/COMPLETESANTACRUZ2.png'
+    },
+    {
+        id: 13,
+        nombre: 'Santa Cruz Classic Dot',
+        descripcion:'Santa Cruz Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+        precio: 34999,
+        imagen: './images/COMPLETESANTACRUZ3.png'
+    },
+    {
+        id: 14,
+        nombre: 'Santa Cruz Dot',
+        descripcion:'Santa Cruz Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+        precio: 34999,
+        imagen: './images/COMPLETESANTACRUZ4.png',
+    },
+    {
+        id: 15,
+        nombre: 'Santa Cruz Flier Dot',
+        descripcion:'Santa Cruz Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+        precio: 34999,
+        imagen: './images/COMPLETESANTACRUZ5.png'
+    },
+    {
+      id: 16,
+      nombre: 'Creature Prowler',
+      descripcion:'Creature Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+      precio: 34999,
+      imagen: './images/COMPLETECREATURE1.png'
+    },
+    {
+      id: 17,
+      nombre: 'Creature Deathcard',
+      descripcion:'Creature Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+      precio: 34999,
+      imagen: './images/COMPLETECREATURE2.png'
+    },
+    {
+      id: 18,
+      nombre: 'Creature Metallic Logo',
+      descripcion:'Creature Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+      precio: 34999,
+      imagen: './images/COMPLETECREATURE3.png'
+    },
+    {
+      id: 19,
+      nombre: 'Creature Mummy Logo',
+      descripcion:'Creature Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+      precio: 34999,
+      imagen: './images/COMPLETECREATURE4.png'
+    },
+    {
+      id: 20,
+      nombre: 'Creature Large Logo',
+      descripcion:'Creature Complete, Skateboard pre armado con los mejores productos de la marca, Hecha de 7 capas de maple canadiense',
+      precio: 34999,
+      imagen: './images/COMPLETECREATURE5.png'
+    },
+      
+  ];
+
+  //Nuevas pruebas para crear el carrito de compras con Bootstrap
+  
+  let carrito = [];  //clase 10 JSON y LOCAL STORAGE este voy a usar para mandar el carrito al local storage
+  const divisa = '$';   //aca creo el signo pesos para agregar a los precios de los skates
+  const DOMitems = document.querySelector('#items'); //constante DOMitems es el contenido dentro del id #items
+  const DOMcarrito = document.querySelector('#carrito'); //constante DOMcarrito es el contenido dentro del id #carrito
+  const DOMtotal = document.querySelector('#total'); //constante DOMtotal es el contenido dentro del id #total
+  const DOMbotonVaciar = document.querySelector('#boton-vaciar'); //constante DOMbotonVaciar es el contenido dentro del id #boton-vaciar
+
+  
+  
+   // acá abajo creo la FUNCION que me renderiza las tarjetas con sus componentes 
+  
+   function renderizarSkates() {
+    baseDeDatos.forEach((info) => {
+
+        // Tarjeta principal
+        const miNodo = document.createElement('div');  // creo miNodo con elemento div que va a ser el contenedor miNodo
+        miNodo.classList.add('cards');   // con add le agrego la clase cards
+
+        // Body de la tarjeta
+        const miNodoCardBody = document.createElement('div'); // creo miNodoCardBody con elemento div que va a ser un contenedor interno de miNodo
+        miNodoCardBody.classList.add('card-body'); // con add le agrego la clase card-body 
+
+        // Nombres De los Skates
+        const miNodoNombreSkate = document.createElement('h5'); // creo miNodoNombreSkate con elemento h5 que va a ser un contenedor interno de miNodo
+        miNodoNombreSkate.classList.add('card-titulo'); // con add le creo la clase card-titulo 
+        miNodoNombreSkate.textContent = info.nombre; // dentro de cada objeto del array traigo "nombre" para mostrar
+
+        // Imagenes de los skates
+        const miNodoImagen = document.createElement('img'); // creo miNodoImagen con etiqueta img donde se va a agregar el link a la imagen
+        miNodoImagen.classList.add('img-sk');  // le agrego la clase img-fluid que voy a usar en css
+        miNodoImagen.setAttribute('src', info.imagen);  //agrego un atributo src para el link a la imagen y traigo del objeto el array "imagen"
+
+        // Descripcion de los skates
+        const miNodoDescripcion = document.createElement('p'); // creo miNodoDescripcion le agrego un parrafo P (porque sera texto mas largo)
+        miNodoDescripcion.classList.add('descripcion'); // le agrego la clase "descripcion" que voy a usar en css
+        miNodoDescripcion.textContent = info.descripcion; //agrego textcontent que traigo del objeto en el array la "descripcion"
+        
+        // Precio
+        const miNodoPrecio = document.createElement('p');// creo miNodoPrecio en parrafo P (allí meteré el precio)
+        miNodoPrecio.classList.add('precio-text'); // le agrego la clase "precio-text" que voy a usar en css
+        miNodoPrecio.textContent = `${divisa}${info.precio}`; //agrego divisa (creada mas arriba) y traigo del objeto en el array el "precio"
+
+        // Botones para agregar item al carrito
+        const miNodoBoton = document.createElement('button'); // creo miNodoBoton y agrego etiqueta "button"
+        miNodoBoton.classList.add('btn', 'btn-secondary', 'Agregar'); // le agrego clases también de bootstrap para que levante estilos
+        miNodoBoton.textContent = 'Agregar a Carrito';// le pongo texto al boton
+        miNodoBoton.setAttribute('marcaID', info.id);// le agrego atributo "marcaID" y traigo del objeto en el array el "id"
+        
+        
+        miNodoBoton.addEventListener('click', agregarSkateAlCarrito); // le agrego un event listener click asociado a la funcion "agregarSkateAlCarrito"
+  
+        miNodoCardBody.append(miNodoImagen); //dentro de "miNodoCardBody" hago el append de la imagen
+        miNodoCardBody.append(miNodoNombreSkate); //dentro de "miNodoCardBody" hago el append del nombre del skate
+        miNodoCardBody.append(miNodoDescripcion); //dentro de "miNodoCardBody" hago el append de la descripción
+        miNodoCardBody.append(miNodoPrecio); //dentro de "miNodoCardBody" hago el append del precio
+        miNodoCardBody.append(miNodoBoton); //dentro de "miNodoCardBody" hago el append del boton
+        miNodo.append(miNodoCardBody); //dentro de "miNodo" hago el append de todo el "miNodoCardBody" con todos sus componentes (los 5 de arriba)
+        DOMitems.append(miNodo); // acá hago el append de "miNodo" con todos sus componentes (miNodoCardBody y sus 5 componentes)
+    });
+  }
+  
+    // Abajo genero evento para añadir un producto al carrito de la compra
+ 
+  function agregarSkateAlCarrito(evento) {   //la funcion de "agregarSkateAlCarrito" se ejecuta a partir 
+                                                //del click en el boton "agregar a carrito" y va a hacer push al array "carrito" del 
+                                                //item seleccionado a patir del atributo 'marcaID', que creamos en la funcion "renderizarSkates"
+   carrito.push(evento.target.getAttribute('marcaID'))
+    
+
+
+
+  Swal.fire({
+    width: "350px",    
+    heightAuto: true,
+    title: 'Item agregado!',   
+    imageUrl: './images/logo2.png',
+    imageWidth: 250,
+    imageHeight: 125,
+    padding: '1%',
+    color: '#ded7e6',
+    background: '#48464b',
+    showConfirmButton: false,
+    timer: 1500
+    
+  })
+
+    
+   renderizarCarrito();  
+
+   
+   localStorage.setItem("productosCarrito", JSON.stringify(carrito));
+     
+  }
+
+        
+  //la función "renderizarCarrito" se ejecuta luego de agregar o quitar items del carrito (la podría haber llamado actualizar carrito)
+  function renderizarCarrito() {     
+    DOMcarrito.innerHTML = '<h2>Carrito de Compras</h2>'; //creo el h2 con el título 
+
+    //creo array carritoSinDuplicados con un "spread" (...) que imprime una lista de argumentos
+    const carritoSinDuplicados = [...new Set(carrito)];  //set es unique (no repite)  // USO DE SPREAD "CLASE 12. Operadores Avanzados"
+    
+    carritoSinDuplicados.forEach((item) => {        //para cada argumento......
+        
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {   //generamos const "miItem" que filtra el objeto del array..
+          // ¿Coincide las id? Solo puede existir un caso           
+            return itemBaseDatos.id === parseInt(item); //y trae el ID que lo parseamos (parseInt) nos devuelve el numero entero.
+            
+        });
+        
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => { // Cuenta el número de veces que se repite el producto
+            
+            return itemId === item ? total += 1 : total;  // ¿si itemid es igual a item entonces sumo 1(incremento el contador),
+        }, 0);  //el contador arranca desde cero
+      
+
+        
+        // Creamos el nodo del item del carrito
+        const miNodo = document.createElement('li');  // creamos minodo con un tag li (lista)
+        
+        miNodo.classList.add('group-item-secondary', 'text-right', 'mx-auto');  // creamos clases a ese li (lista)
+        // le ponemos lo que queremos mostrar en el carrito
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} ${divisa} ${miItem[0].precio}  `; 
+
+      ////////////acá va la creación del boton Pagar y su función////////////
+       //////////////////////////
+       //////////////////////////
+       //////////////////////////
+       //////////////////////////
+       //////////////////////////
+       //////////////////////////
+       //////////////////////////
+       //////////////////////////
+
+
+
+
+        // Creo boton de borrar (dentro del nodo) con Tag "button"
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-secondary', 'mx-5'); //agrego las clases para bootstrap
+        miBoton.textContent = 'Quitar';  //agrego el texto del boton
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+
+        miNodo.append(miBoton); //imprimo el boton dentro del nodo
+        DOMcarrito.append(miNodo); // imprimo miNodo con boton incluido
+       
+    });
+    
+    DOMtotal.textContent = calcularTotal(); //ejecuto la función calcularTotal que se imprime en #total
+    
+
+  }
+  
+  //Evento para borrar un elemento del carrito
+  function borrarItemCarrito(evento) { 
+    
+    const id = evento.target.dataset.item;  // Obtenemos el producto ID
+    
+    carrito = carrito.filter((carritoId) => {  // Borramos
+        return carritoId !== id;
+    });
+    
+    renderizarCarrito();// volvemos a renderizar
+  }
+  
+  function calcularTotal() {
+    // Recorremos el array del carrito 
+    return carrito.reduce((total, item) => {  
+         // De cada elemento obtengo su precio
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Los sumo al total
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
+  }
+  
+  function vaciarCarrito() {
+    // Limpiamos los productos guardados
+    carrito = [];
+    // Renderizo los cambios
+    renderizarCarrito();
+  }
+  // Este es event listener de vaciar el carrito completo (vaciarCarrito)
+  DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+  
+  renderizarSkates(); 
+  renderizarCarrito();
---
+++ js/storage.js
@@ -0,0 +1,94 @@
+
+
+// Variables
+let nombreUsuario = localStorage.getItem('nombreUsuario');
+let correoUsuario = localStorage.getItem('correoUsuario');
+let edadUsuario = localStorage.getItem('edadUsuario');
+
+
+
+
+// Variables DOM
+const saludoInicial = document.querySelector('#saludoUsuario');
+const formulario = document.querySelector('#formulario');
+const nombre = document.querySelector('#nombre');
+const correo = document.querySelector('#correo');
+const edad = document.querySelector('#edad');
+const contFormulario = document.querySelector('#contFormulario');
+const contTienda = document.querySelector("#contenido"); //este quiero ocultar
+const contenid = document.querySelector('#contenid');
+const logout = document.querySelector('#logout');
+
+saludoInicial.innerHTML = "<h3>Bienvenido a nuestro paraiso skater!!, por favor ingresa los siguientes datos para continuar!</h3>";
+
+//Funciones
+
+
+const ocultarTienda = () => {
+    contTienda.style.display = "none";
+      }
+
+ocultarTienda()  // Oculto la tienda antes de que llene el formulario
+
+const ocultarFormulario = () => {
+    contFormulario.style.display = 'none';
+    contTienda.style.display = "block"; // muestro la tienda cuando llena el formulario
+    contenid.innerHTML = `Hola "${nombreUsuario}" tu mail es: "${correoUsuario}". Tienes ${edadUsuario} años.`;
+  }
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    nombreUsuario = nombre.value;
+    correoUsuario = correo.value;
+    edadUsuario = edad.value;
+
+
+    if(nombreUsuario === "") {   // acá valido que ponga nombre en el form
+      alert('No has escrito nada en el usuario');
+      return;
+    }
+    else if(edadUsuario === "") {    // acá valido que ponga edad en el form
+     alert('No has escrito una edad');
+     return;
+    }
+    else if(correoUsuario === "") {    // acá valido que ponga su correo en el form
+      alert('No has ingresado tu correo');
+      return;
+    }
+
+    localStorage.setItem('nombreUsuario', nombre.value);   //escribo en el local storage
+    localStorage.setItem('edadUsuario', edad.value);
+    localStorage.setItem('correoUsuario', correo.value);
+    
+    ocultarFormulario();   //disparo esta función
+  })
+
+  if (nombreUsuario && correoUsuario && edadUsuario) {    //operador Lógico AND (&&)
+    ocultarFormulario();
+  }
+
+  logout.onclick = () => {  // función para el boton de logout
+    // localStorage.clear();
+    localStorage.removeItem('nombreUsuario');
+    localStorage.removeItem('correoUsuario');
+    localStorage.removeItem('edadUsuario');
+    location.reload() // esto recarga la página
+  }
+
+//  STRINGIFY + SUBIR AL LOCAL STORAGE
+  
+localStorage.setItem("productosDisponibles", JSON.stringify(baseDeDatos)); //acá guardo todos los productos disponibles en el local storage
+localStorage.setItem("productosCarrito", JSON.stringify(carrito));
+
+//  TRAER DEL LOCAL STORAGE +  PARSE
+
+const nuevoArrayDeSkates = JSON.parse(localStorage.getItem("productosDisponibles"));
+console.log(nuevoArrayDeSkates);
+
+const CarritoRecuperado = JSON.parse(localStorage.getItem("carritoLocalStorage"));
+console.log(CarritoRecuperado);
+ 
+let carritoLocalStorage = JSON.parse( localStorage.getItem('productosCarrito'))
+  console.log(carritoLocalStorage);
+  
